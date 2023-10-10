415116324101c30f4dcbb55c3d7b523644a7f8e2
M3rg <123715188+Mergezy@users.noreply.github.com>
2023-10-11 00:42:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,23 @@
-import java.util.Scanner;
+public class Singleton {
+    private static Singleton instance;
+    private final double result;
 
-public class Main {
-    private int x;
-    private int y;
-
-    public Main(int x, int y) {
-        this.x = x;
-        this.y = y;
+    private Singleton(double x, double y) {
+        this.result = 2 * x + 3 / y;
     }
 
-    public double calculateExpression() {
-        if (y != 0) {
-            return 2 * x + 3.0 / y; // Обратите внимание, что 3 делится на double, чтобы избежать целочисленного деления
-        } else {
-            System.out.println("Ошибка: деление на ноль");
-            return Double.NaN; // Возврат NaN (Not-a-Number) в случае деления на ноль
+    public double getResult() {
+        return result;
+    }
+
+    public static Singleton getInstance(double x, double y) {
+        if (instance == null) {
+            instance = new Singleton(x, y);
         }
+        return instance;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите значение x (целое число): ");
-        int x = scanner.nextInt();
-
-        System.out.print("Введите значение y (целое число): ");
-        int y = scanner.nextInt();
-
-        // Создаем объект класса с введенными значениями x и y
-        Main calculator = new Main(x, y);
-
-        // Вычисляем и выводим значение выражения
-        double result = calculator.calculateExpression();
-        System.out.println("Значение выражения: " + result);
+        System.out.println(Singleton.getInstance(100, 3).getResult());
     }
 }
